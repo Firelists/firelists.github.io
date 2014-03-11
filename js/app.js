895e25329015aff6eb8aa428026597c409047d4b
Andreas McDermott <andreasmcdermott@users.noreply.github.com>
2014-03-11 15:51:23 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -220,7 +220,7 @@ App.AuthController = Ember.Controller.extend({
         var properties = {
           ref: userRef,
           id: user.uid,
-          username: user.displayName
+          username: user.provider === 'google' ? user.email : user.username
         };
         var controller = this;
         userRef.once('value', function (snapshot) {
